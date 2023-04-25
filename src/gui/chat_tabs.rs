9c2e8c91f1846cf6456b8877ce6a4bc2aea79268
TicClick <ya@ticclick.ch>
2023-04-25 20:31:17 +0200

    add scroll bars for open channel/chat list

--- src/gui/chat_tabs.rs
+++ src/gui/chat_tabs.rs
@@ -12,6 +12,7 @@ use crate::gui::state::UIState;
 pub struct ChatTabs {
     pub new_channel_input: String,
     pub new_chat_input: String,
+    chat_row_height: Option<f32>,
 }
 
 impl ChatTabs {
@@ -103,7 +104,7 @@ impl ChatTabs {
         });
     }
 
-    fn show_chats(&self, state: &mut UIState, ui: &mut Ui, mode: ChatType) {
+    fn show_chats(&mut self, state: &mut UIState, ui: &mut Ui, mode: ChatType) {
         let it: Vec<(String, String, ChatState)> = state
             .filter_chats(|ch| match mode {
                 ChatType::Channel => ch.name.is_channel(),
@@ -118,67 +119,77 @@ impl ChatTabs {
             })
             .collect();
 
+        let chat_row_height = *self.chat_row_height.get_or_insert_with(|| {
+            ui.text_style_height(&egui::TextStyle::Body) + 2. * ui.spacing().item_spacing.y
+        });
+        let area_height = chat_row_height * it.len().clamp(0, 10) as f32;
+
         let mut chats_to_clear = BTreeSet::new();
+        egui::ScrollArea::vertical()
+            .id_source(format!("{mode}-tabs"))
+            .auto_shrink([false, true])
+            .max_height(area_height)
+            .show(ui, |ui| {
+                for (normalized_chat_name, chat_name, chat_state) in it {
+                    // ui.vertical(|ui| {
+                    ui.horizontal(|ui| {
+                        let mut label = egui::RichText::new(&chat_name);
+                        label = label.color(pick_tab_colour(state, &normalized_chat_name));
 
-        for (normalized_chat_name, chat_name, chat_state) in it {
-            ui.vertical(|ui| {
-                ui.horizontal(|ui| {
-                    let mut label = egui::RichText::new(&chat_name);
-                    label = label.color(pick_tab_colour(state, &normalized_chat_name));
+                        let chat_tab = ui.selectable_value(
+                            &mut state.active_chat_tab_name,
+                            normalized_chat_name.to_owned(),
+                            label,
+                        );
+                        if chat_tab.clicked() {
+                            state.highlights.mark_as_read(&normalized_chat_name);
+                        }
+                        if matches!(chat_state, ChatState::JoinInProgress) {
+                            ui.spinner();
+                        }
+                        if chat_tab.middle_clicked() {
+                            state.core.chat_tab_closed(&normalized_chat_name);
+                        }
 
-                    let chat_tab = ui.selectable_value(
-                        &mut state.active_chat_tab_name,
-                        normalized_chat_name.to_owned(),
-                        label,
-                    );
-                    if chat_tab.clicked() {
-                        state.highlights.mark_as_read(&normalized_chat_name);
-                    }
-                    if matches!(chat_state, ChatState::JoinInProgress) {
-                        ui.spinner();
-                    }
-                    if chat_tab.middle_clicked() {
-                        state.core.chat_tab_closed(&normalized_chat_name);
-                    }
-
-                    chat_tab.context_menu(|ui| {
-                        if matches!(mode, ChatType::Channel) {
-                            if state.settings.chat.autojoin.contains(&normalized_chat_name) {
-                                if ui.button("Remove from favourites").clicked() {
-                                    state.settings.chat.autojoin.remove(&normalized_chat_name);
+                        chat_tab.context_menu(|ui| {
+                            if matches!(mode, ChatType::Channel) {
+                                if state.settings.chat.autojoin.contains(&normalized_chat_name) {
+                                    if ui.button("Remove from favourites").clicked() {
+                                        state.settings.chat.autojoin.remove(&normalized_chat_name);
+                                        // TODO: this should be done elsewhere, in a centralized manner, I'm just being lazy right now
+                                        state.core.settings_updated(&state.settings);
+                                        ui.close_menu();
+                                    }
+                                } else if ui.button("Add to favourites").clicked() {
+                                    state
+                                        .settings
+                                        .chat
+                                        .autojoin
+                                        .insert(normalized_chat_name.to_owned());
                                     // TODO: this should be done elsewhere, in a centralized manner, I'm just being lazy right now
                                     state.core.settings_updated(&state.settings);
                                     ui.close_menu();
                                 }
-                            } else if ui.button("Add to favourites").clicked() {
-                                state
-                                    .settings
-                                    .chat
-                                    .autojoin
-                                    .insert(normalized_chat_name.to_owned());
-                                // TODO: this should be done elsewhere, in a centralized manner, I'm just being lazy right now
-                                state.core.settings_updated(&state.settings);
+                            }
+
+                            if ui.button("Clear messages").clicked() {
+                                chats_to_clear.insert(normalized_chat_name.to_owned());
                                 ui.close_menu();
                             }
-                        }
 
-                        if ui.button("Clear messages").clicked() {
-                            chats_to_clear.insert(normalized_chat_name.to_owned());
-                            ui.close_menu();
-                        }
-
-                        let close_title = match mode {
-                            ChatType::Channel => "Leave",
-                            ChatType::Person => "Close",
-                        };
-                        if ui.button(close_title).clicked() {
-                            state.core.chat_tab_closed(&normalized_chat_name);
-                            ui.close_menu();
-                        }
+                            let close_title = match mode {
+                                ChatType::Channel => "Leave",
+                                ChatType::Person => "Close",
+                            };
+                            if ui.button(close_title).clicked() {
+                                state.core.chat_tab_closed(&normalized_chat_name);
+                                ui.close_menu();
+                            }
+                        });
                     });
-                });
+                    // });
+                }
             });
-        }
 
         for target in chats_to_clear {
             state.clear_chat(&target);
