5256cadee25d4381bb6958cdb43e98d6288638dd
TicClick <ya@ticclick.ch>
2023-05-16 15:56:37 +0200

    allow `#` as a highlight symbol

--- crates/steel_core/src/chat/mod.rs
+++ crates/steel_core/src/chat/mod.rs
@@ -110,17 +110,17 @@ impl Message {
     }
 
     pub fn detect_highlights(&mut self, keywords: &BTreeSet<String>) {
-        let separator = "##";
+        let separator = "$$";
         let normalized_text = self
             .text
             .to_lowercase()
-            .split(|ch: char| ch.is_whitespace() || (ch.is_ascii_punctuation() && !matches!(ch, '[' | ']')))
+            .split(|ch: char| ch.is_whitespace() || (ch.is_ascii_punctuation() && !matches!(ch, '[' | ']' | '#')))
             .collect::<Vec<&str>>()
             .join(separator);
         let normalized_text = format!("{separator}{normalized_text}{separator}");
 
         for keyword in keywords {
-            let keyword = format!("{separator}{}{separator}", keyword.replace(' ', "##"));
+            let keyword = format!("{separator}{}{separator}", keyword.replace(' ', separator));
             if normalized_text.contains(&keyword) {
                 self.highlight = true;
                 break;
