1329add26ea06a30a375255fd074de5f6bfd1d7a
TicClick <ya@ticclick.ch>
2024-01-12 23:10:57 +0100

    remove remaining mentions of plugins following #50

--- crates/steel_core/src/settings/application.rs
+++ crates/steel_core/src/settings/application.rs
@@ -3,14 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
 pub struct Application {
     #[serde(default)]
-    pub plugins: Plugins,
-    #[serde(default)]
     pub autoupdate: AutoUpdate,
-}
-
-#[derive(Clone, Debug, Default, Serialize, Deserialize)]
-pub struct Plugins {
-    pub enabled: bool,
 }
 
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
